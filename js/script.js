ea2f55c1512c830e5e7e29a401817046c3e4eb98
Vlad <vladyslavyantsov@gmail.com>
2018-04-07 16:36:43 +0200

    drop menu done

--- js/script.js
+++ js/script.js
@@ -5,3 +5,21 @@
 //        scrollTop: $('main').offset().top
 //    }, 1000);
 //})
+
+
+let hamburger = document.querySelector('.hamburger_box');
+let menu = document.querySelector('header nav ul');
+let listItem = menu.lastElementChild;
+
+function showMenu() {
+    menu.style.display === 'block';
+    if (menu.style.display === 'none') {
+        let clonedListItem = listItem.cloneNode(false);
+        clonedListItem.innerHTML = '<a href="projects.html">Projects</a>';
+        menu.style.display = 'block';
+    } else{
+        menu.style.display = 'none';
+    }
+}
+
+hamburger.addEventListener('click', showMenu);
